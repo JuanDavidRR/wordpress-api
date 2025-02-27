4ba58f7ddaea649edfdc981fd3e9e295a64c2ae9
David <juandavidrom@outlook.com>
2025-02-27 17:47:54 -0500

    Update menu items

--- src/data/index.ts
+++ src/data/index.ts
@@ -5,26 +5,26 @@ export const mainMenu = [
   },
   {
     title: 'Politics',
-    url: 'http://localhost:4321/category/politics'
+    url: 'https://wordpress-api-eight.vercel.app/category/politics'
   },
   {
     title: "Industry Insight",
-    url: "http://localhost:4321/category/industry-insight",
+    url: "https://wordpress-api-eight.vercel.app/category/industry-insight",
   },
   {
     title: "Announcements",
-    url: "http://localhost:4321/category/announcements",
+    url: "https://wordpress-api-eight.vercel.app/category/announcements",
   },
   {
     title: 'Music',
-    url: 'http://localhost:4321/category/music'
+    url: 'https://wordpress-api-eight.vercel.app/category/music'
   },
   {
     title: "Security Tips",
-    url: "http://localhost:4321/category/security-tips",
+    url: "https://wordpress-api-eight.vercel.app/category/security-tips",
   },
   {
     title: "Threats Trends",
-    url: "http://localhost:4321/category/threats-trends",
+    url: "https://wordpress-api-eight.vercel.app/category/threats-trends",
   },
 ];
